3b6bec411fc1f0f26bea690bf1eb77de155ae80e
o_panda_o <emailofpanda@yahoo.com>
2019-01-09 22:49:42 +0530

    Second day commit

--- src/main/java/xyz/theefficientcodes/HibernateTest.java
+++ src/main/java/xyz/theefficientcodes/HibernateTest.java
@@ -6,23 +6,60 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import xyz.theefficientcodes.dto.Address;
 import xyz.theefficientcodes.dto.UserDetails;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
 		UserDetails user=new UserDetails();
+		Address address=new Address();
+		
+		address.setCity("Berhampur");
+		address.setState("Odisha");
+		address.setStreet("Saradhabali Housing Complex");
+		address.setPincode("760010");
+		
 		user.setUserId(1);
 		user.setUserName("Bibhuti");
-		user.setAddress("Earth");
+		user.setHomeAddress(address);
+		user.setOfficeAddress(address);
 		user.setJoinedDate(new Date());
 		user.setDescription("Hot Guy");
+		user.getGfAddresses().add(address);
+		
+		UserDetails user2=new UserDetails();
+		Address address2=new Address();
+		
+		address2.setCity("Berhampur");
+		address2.setState("Odisha");
+		address2.setStreet("Saradhabali Housing Complex");
+		address2.setPincode("760010");
+		
+		user2.setUserName("Bonty");
+		user2.setHomeAddress(address2);
+		user2.setOfficeAddress(address2);
+		user2.setJoinedDate(new Date());
+		user2.setDescription("Hot Guy");
+		user2.getGfAddresses().add(address);
+		user2.getGfAddresses().add(address2);
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
+		session.save(user2);
 		session.getTransaction().commit();
+		session.close();
+		
+		user=null;
+		
+		session=sessionFactory.openSession();
+		session.beginTransaction();
+		user=(UserDetails)session.get(UserDetails.class, 1);
+		System.out.println("Username retrieved is "+user.getUserName()+" and description is "+user.getDescription());
+		session.close();
+		
 	}
 
 }
--- src/main/java/xyz/theefficientcodes/dto/UserDetails.java
+++ src/main/java/xyz/theefficientcodes/dto/UserDetails.java
@@ -9,46 +9,83 @@
  */
 package xyz.theefficientcodes.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
+// Imports non-compliant with JPA
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+
 @Entity
 // @Entity (name="USER_DETAILS") // Changes the name of whole entity(HQL will clarify)
 @Table (name="USER_DETAILS")
 public class UserDetails {
-	
+
 	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE) // Generate primary key, parameters are optional
 	@Column(name="id")
 	private int userId;	
+	
 	@Column(name="name",nullable=false)
 	private String userName;
+	
 	@Temporal(TemporalType.DATE) // Only stores the date instead of entire time stamp
 	private Date joinedDate;
-	private String address;
+	
+	@Embedded // Not mandatory if the class is annotated(but good practice)
+	@AttributeOverrides({
+		@AttributeOverride(name="street",column=@Column(name="HOME_STREET_NAME")),
+		@AttributeOverride(name="city",column=@Column(name="HOME_CITY_NAME")),
+		@AttributeOverride(name="state",column=@Column(name="HOME_STATE_NAME")),
+		@AttributeOverride(name="pincode",column=@Column(name="HOME_PIN_CODE"))
+	})
+	
+	private Address homeAddress;
+	
+	@Embedded // Not mandatory if the class is annotated(but good practice)
+	private Address officeAddress;
+
+	// Used list instead of set because it supports indexing
+	@ElementCollection
+	@JoinTable(
+			name="gfAddresses",
+			joinColumns=@JoinColumn(name="USER_ID")
+	)	
+	@GenericGenerator(name = "inc-gen", strategy = "increment")
+	@CollectionId(columns = { @Column(name="address_id") }, generator = "inc-gen", type = @Type(type="long"))
+	private Collection<Address> gfAddresses=new ArrayList<Address>();
+	
 	@Lob
 	private String description; // Store very large object(more than 255 characters)
+	
 	@Transient
 	private boolean isIdiot; // this field will not be added to the database
-	
+
 	public Date getJoinedDate() {
 		return joinedDate;
 	}
 	public void setJoinedDate(Date joinedDate) {
 		this.joinedDate = joinedDate;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
 	}
 	public String getDescription() {
 		return description;
@@ -68,5 +105,30 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	public boolean isIdiot() {
+		return isIdiot;
+	}
+	public void setIdiot(boolean isIdiot) {
+		this.isIdiot = isIdiot;
+	}
+	public Address getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public Address getOfficeAddress() {
+		return officeAddress;
+	}
+	public void setOfficeAddress(Address officeAddress) {
+		this.officeAddress = officeAddress;
+	}
+	public Collection<Address> getGfAddresses() {
+		return gfAddresses;
+	}
+	public void setGfAddresses(Collection<Address> gfAddresses) {
+		this.gfAddresses = gfAddresses;
+	}
 	
+
 }
